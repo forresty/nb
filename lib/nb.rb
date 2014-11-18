11a2bbd6c19931a9d12613bf629868f267c47269
Forrest Ye <afu@forresty.com>
2014-11-18 15:03:43 +0800

    probs

--- lib/nb.rb
+++ lib/nb.rb
@@ -1,12 +1,61 @@
 class NaiveBayes
   VERSION = "0.0.1"
 
-  def initialize(categories = [])
+  def initialize(*categories)
     @categories = []
+    @tokens_count = {}
+    @categories_count = {}
+
+    categories.each do |category|
+      @tokens_count[category] = Hash.new(0)
+      @categories_count[category] = 0
+    end
   end
 
-  def train(category, *features)
+  def train(category, *tokens)
+    tokens.uniq.each do |token|
+      @tokens_count[category][token] += 1
+    end
+    @categories_count[category] += 1
+  end
+
+  def classify(tokens)
     raise 'not implemented yet'
+  end
+
+  def classifications(tokens)
+    raise 'not implemented yet'
+  end
+
+  def probability_of_a_token_given_a_category(token, category)
+    return assumed_probability if @tokens_count[category][token] == 0
+
+    @tokens_count[category][token].to_f / @categories_count[category]
+  end
+
+  def probability_of_tokens_given_a_category(tokens, category)
+    tokens.inject(1.0) do |product, token|
+      product * probability_of_a_token_given_a_category(token, category)
+    end
+  end
+
+  def probability_of_a_category(category)
+    @categories_count[category].to_f / total_number_of_items
+  end
+
+  # def total_number_of_tokens
+  #   @tokens_count.values.inject(0) { |sum, hash| sum + hash.values.inject(&:+) }
+  # end
+
+  def total_number_of_items
+    @categories_count.values.inject(&:+)
+  end
+
+  # If we have only trained a little bit a class may not have had a feature yet
+  # give it a probability of 0 may not be true so we produce a assumed probability
+  # which gets smaller more we train
+  def assumed_probability
+    0.5 / (total_number_of_items / 2)
   end
 
   def save
--- spec/nb_spec.rb
+++ spec/nb_spec.rb
@@ -3,6 +3,66 @@ require "spec_helper"
 describe NaiveBayes do
   it { should respond_to :train }
   it { should respond_to :save }
+  it { should respond_to :classify }
+  it { should respond_to :classifications }
+  it { should respond_to :probability_of_a_token_given_a_category }
+  it { should respond_to :probability_of_tokens_given_a_category }
+  it { should respond_to :probability_of_a_category }
+  # it { should respond_to :total_number_of_tokens }
+  it { should respond_to :total_number_of_items }
+
+  let(:bayes) { NaiveBayes.new(:love, :hate) }
+  subject { bayes }
+
+  # describe '#total_number_of_tokens' do
+  #   it 'calculates correctly' do
+  #     bayes.train :love, 'I', 'love', 'you'
+  #     bayes.train :hate, 'I', 'hate', 'you'
+  #
+  #     bayes.total_number_of_tokens.should == 6
+  #
+  #     bayes.train :love, 'I', 'love', 'you', 'more'
+  #
+  #     bayes.total_number_of_tokens.should == 10
+  #   end
+  # end
+
+  describe '#total_number_of_items' do
+    it 'calculates correctly' do
+      bayes.train :love, 'I', 'love', 'you'
+      bayes.train :hate, 'I', 'hate', 'you'
+
+      bayes.total_number_of_items.should == 2
+
+      bayes.train :love, 'I', 'love', 'you', 'more'
+
+      bayes.total_number_of_items.should == 3
+    end
+  end
+
+  describe '#probability_of_a_category' do
+    it 'calculates correctly' do
+      bayes.train :love, 'I', 'love', 'you'
+      bayes.train :hate, 'I', 'hate', 'you'
+
+      bayes.probability_of_a_category(:love).should == 0.5
+    end
+  end
+
+  describe '#probability_of_token_given_a_category' do
+    it 'calculates correctly' do
+      bayes.train :love, 'I', 'love', 'you'
+      bayes.train :hate, 'I', 'hate', 'you'
+
+      bayes.probability_of_a_token_given_a_category('love', :love).should == 1
+      bayes.probability_of_a_token_given_a_category('you', :hate).should == 1
+
+      bayes.train :love, 'I', 'love', 'you', 'more'
+
+      bayes.probability_of_a_token_given_a_category('more', :love).should == 0.5
+      # bayes.probability_of_token_given_a_category('more', :hate).should == 0
+    end
+  end
 
   describe 'class methods' do
     subject { NaiveBayes }
